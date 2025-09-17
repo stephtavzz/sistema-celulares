62a612753df04bbfd3b8e55b52c79535318efd8f
stephtavzz <stephanie.tavares.pe@gmail.com>
2025-09-17 13:36:38 -0300

    Sistema de celulares

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿﻿using SistemaCelulares.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "99999-0000", modelo: "Nokia 3310", imei: "111111111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\nSmartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "98888-0000", modelo: "iPhone 13", imei: "222222222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
